b9924c72d9b4c7cf4e8ad423ba0847b34a06e4f1
Ethanscharlie <ethanscharlie@protonmail.com>
2025-11-15 01:15:16 -0700

    Added AppUserInterface method stubs

--- src/main/java/io/github/theballwizards/AppUserInterface.java
+++ src/main/java/io/github/theballwizards/AppUserInterface.java
@@ -1,4 +1,20 @@
 package io.github.theballwizards;
 
 public class AppUserInterface {
+    private Runnable searchCallback;
+
+    public AppUserInterface() {
+
+    }
+
+    /**
+     * Sets the search callback member.
+     * Uses the builder pattern.
+     * @param callback Function to run when the go button is pressed.
+     * @return instance of caller.
+     */
+    public AppUserInterface setSearchCallback(Runnable callback) {
+        searchCallback = callback;
+        return this;
+    }
 }
