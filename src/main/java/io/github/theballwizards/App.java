ce6c85e6d3242c97543265a717b46740a7c2d270
Ethanscharlie <ethanscharlie@protonmail.com>
2025-11-15 00:36:54 -0700

    Added function stubs to app

--- src/main/java/io/github/theballwizards/App.java
+++ src/main/java/io/github/theballwizards/App.java
@@ -1,6 +1,21 @@
 package io.github.theballwizards;
 
+import edu.princeton.cs.algs4.Digraph;
+
 public class App {
     public static void main(String[] args) {
+    }
+
+    /**
+     * Finds a path of urls between two articles through a wiki.
+     * Articles must have the same web domain.
+     * Warning, this may take a while.
+     * @param graph A directional graph containing every url on the site
+     * @param startUrl The starting position
+     * @param endUrl The target position
+     * @return An iterable of urls, starting at the start, and ending at the end.
+     */
+    private Iterable<String> findPath(Digraph graph, String startUrl, String endUrl) {
+        return null; // TODO
     }
 }
